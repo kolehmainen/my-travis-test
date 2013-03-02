8723c47926a197f97b10ee994de38d9c86e7a38b
Jussi Kolehmainen <jussi.kolehmainen@hiq.fi>
2013-03-02 15:21:39 +0200

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyTravisTest::Application.config.secret_token = 'c2fbd0cd719f02b60479cf65eab4109f81ed43e5af3927b49a860d2514c22859765ee818b381238f297930518e016d43f12b0982243de9f4fef22195a7fbdc0f'
